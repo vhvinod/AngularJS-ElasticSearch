6036d1d94a3c51fe4e3412b525db3ee454e8ecd8
vhvinod <vh.vinod@gmail.com>
2016-10-26 22:23:38 +0530

    Fixed Exact Match Issue #3 (Fixed Auto Completed issue)

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -16,7 +16,7 @@ app.controller('mainController', function ($scope, elasticClient) {
             body: {
                 'query': {
                     'query_string': {
-                        'query': $scope.search.queryTerm
+                        'query': $scope.search.queryTerm+'*'
                     }
                 }
             }
